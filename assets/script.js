98defd25de5edbbc473aa526809ec91705fe7826
Zach <zjsievers@gmail.com>
2020-03-21 15:05:43 -0400

    api call nyt

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,57 @@
-var settings = {
-	"async": true,
-	"crossDomain": true,
-	"url": "https://coronavirus-monitor.p.rapidapi.com/coronavirus/cases_by_country.php",
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "coronavirus-monitor.p.rapidapi.com",
-		"x-rapidapi-key": "cd41e7ceecmshaa77971b674259cp1cbd8ejsne9ded19a3f9e"
-	}
+// var settings = {
+// 	"async": true,
+// 	"crossDomain": true,
+// 	"url": "https://coronavirus-monitor.p.rapidapi.com/coronavirus/cases_by_country.php",
+// 	"method": "GET",
+// 	"headers": {
+// 		"x-rapidapi-host": "coronavirus-monitor.p.rapidapi.com",
+// 		"x-rapidapi-key": "cd41e7ceecmshaa77971b674259cp1cbd8ejsne9ded19a3f9e"
+// 	}
+// }
+
+// $.ajax(settings).done(function (response) {
+// 	console.log(response);
+// });
+
+console.log("loaded");
+
+
+// // ajax call for ny times api
+
+
+function buildQueryUrl() {
+    var queryURL = "https://cors-anywhere.herokuapp.com/https://api.nytimes.com/svc/search/v2/articlesearch.json?q=coronavirus&api-key=tRkWuiJbzNglNxZ4AHhGjmuVkPBAF7QR";
+    queryParams.q = $("#search")
+
 }
 
-$.ajax(settings).done(function (response) {
-	console.log(response);
+
+
+
+
+
+
+
+
+
+
+
+ $("#submit-btn").on("click", function() {
+
+
+    var queryURL = "https://cors-anywhere.herokuapp.com/https://api.nytimes.com/svc/search/v2/articlesearch.json?q=coronavirus&api-key=tRkWuiJbzNglNxZ4AHhGjmuVkPBAF7QR";
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response) {
+          console.log(response);
+
+          var headline = $("<h1>").text(response.docs.headline);
+          var 
+          
+    });
 });
+
+
+// https://api.nytimes.com/svc/search/v2/articlesearch.json?q=coronvirus&api-key=tRkWuiJbzNglNxZ4AHhGjmuVkPBAF7QR
