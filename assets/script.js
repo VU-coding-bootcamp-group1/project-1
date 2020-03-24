11e29a0decd69e83e4960726f97d0da06f97400a
Zach <zjsievers@gmail.com>
2020-03-23 21:16:44 -0400

    html stats div table

--- assets/script.js
+++ assets/script.js
@@ -31,41 +31,8 @@ for (var i = 0; i < countries.length; i++) {
         }) 
     };     
 
-    // function displayData() {
-    //     $("#submit").on("click", )
-    // }
-
-// 1. country typed click of submit button
-// 2. jquery grabbing search input and using .text to place it in display div
-// 3. deaths, cases is pulled from country that is chosen
-// 4. 
 
 
-// $("#searchForm").text
-
-
-
-
-
-// ajax call for virus tracker api
-// for loop to go through array l.17   
-
-    // function buildQueryUrl() {
-    //    var queryURL = "https://corona.lmao.ninja/countries"
-    //     $.ajax({
-    //         url: queryURL,
-    //         dataType: 'json',
-    //         success: function(data) {
-    //           console.log(data);
-    //           for (var i = 0; i < data.length; i++) {
-                  
-    //           }
-    //           console.log(data[195]);
-    //         }
-    //       });
-    // }
-
-    // buildQueryUrl();
 
 
 // // ajax call for ny times api
@@ -98,17 +65,3 @@ for (var i = 0; i < countries.length; i++) {
 //     }
 // }
 
-
-
-
-
-
-
-// https://api.nytimes.com/svc/search/v2/articlesearch.json?q=coronvirus&api-key=tRkWuiJbzNglNxZ4AHhGjmuVkPBAF7QR
-
-
-
-// apiurl1: https://corona.lmao.ninja/countries
-    // data.updated ; data.cases ; data.deaths ; data.recovered ; data country
-                // data.updated => new date(data.updated)
-    // worldometer source
